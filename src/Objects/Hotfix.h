2ce7f129413d9e5576377867d77f298848a6d538
a <sajdokova@gmail.com>
2020-05-07 15:15:57 +0200

    added Hotfix.h

---
+++ src/Objects/Hotfix.h
@@ -0,0 +1,14 @@
+/**
+* @author Anna Sajdokova <sajdoann@fit.cvut.cz>
+* @date 5/7/2020
+*/
+
+#pragma once
+/**
+ * moving object that is emited by patches
+ * it travels on board till it encounters other object, then its destroyed (if encounters virus it takes one live away)
+ */
+class Hotfix{
+
+};
+
